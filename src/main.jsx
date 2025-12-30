eceeb4cb3a0f6078cfd738fdf605ab93e4c80293
sayantaniy <sayantanimanna139@gmail.com>
2025-12-30 21:24:46 +0530

    test on mobile

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <div onPointerDownCapture={() => {}}>
+  <App />
+</div>
   </BrowserRouter>,
 )
